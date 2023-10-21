1b557ec68c19b19bb5d39ab4a81e0b9fae9f2bfd
mustafaad12 <mustafa.ssi.msn121@gmail.com>
2023-10-21 17:40:05 +0300

    push to github++

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,16 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-  Route,
-} from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom";
 //store
 import { store } from "./store.ts";
 import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
+
+//push to github
 
 const router = createBrowserRouter(
   createRoutesFromElements(
